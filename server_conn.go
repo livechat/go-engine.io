62b203ff1214f6746965263944cf70b9b178aabb
kamildrazkiewicz <drazkiewicz.k@gmail.com>
2017-03-21 14:52:16 +0100

    upgrading time incresed

--- server_conn.go
+++ server_conn.go
@@ -134,7 +134,7 @@ func (c *serverConn) NextReader() (MessageType, io.ReadCloser, error) {
 func (c *serverConn) NextWriter(t MessageType) (io.WriteCloser, error) {
 	switch c.getState() {
 	case stateUpgrading:
-		for i := 0; i < 30; i++ {
+		for i := 0; i < 50; i++ {
 			time.Sleep(50 * time.Millisecond)
 			if c.getState() != stateUpgrading {
 				break
